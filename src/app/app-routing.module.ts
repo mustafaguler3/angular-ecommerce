7f25d798ef5d4b5cd6d565c832f78147df138029
mustafaguler3 <mustafa.glr@hotmail.com>
2023-08-12 22:10:01 +0300

    added breadcrumb service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 
 const routes: Routes = [
+  {path:"",component:HomeComponent,data:{breadcrumb:"Home"}},
   {path:"test-error",component:TestErrorComponent},
   {path:"not-found",component:NotFoundComponent},
   {path:"server-error",component:ServerErrorComponent},
-  {path:"",component:HomeComponent},
   {path:"shop",loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
   {path:"**",redirectTo:"",pathMatch:"full"}
 ];
--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,8 @@ export class ProductDetailsComponent implements OnInit{
   product?: Product;
 
   constructor(private shopService:ShopService,
-              private activeRoute: ActivatedRoute){}
+              private activeRoute: ActivatedRoute,
+              private bcService:BreadcrumbService){}
 
   ngOnInit(): void {
     this.loadProduct()
@@ -26,6 +28,7 @@ export class ProductDetailsComponent implements OnInit{
       this.shopService.getProduct(id).subscribe(
         response => {
           this.product = response;
+          this.bcService.set('@productDetails',response.name);
         },err => {
           console.log(err)
         }
